04d9f2bff1e89e49f0d4a4a2fb13ba9da7a13e82
MartinLourduraj <martinlourduraj2018@gmail.com>
2026-09-08 22:19:08 +0530

    Configure Django to use PostgreSQL

--- vmmc_erp/settings.py
+++ vmmc_erp/settings.py
@@ -84,13 +84,12 @@ ASGI_APPLICATION = 'vmmc_erp.asgi.application'
 # Database - PostgreSQL
 # ============================================================
 
-import os
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+    )
 }
 
 # Custom User Model
