0cf9d8cdcfb48650232189b6c413fbff403034e2
MartinLourduraj <138987908+MartinLourduraj@users.noreply.github.com>
2026-09-09 16:57:07 +0530

    Temp debug dashboard

--- apps/core/views.py
+++ apps/core/views.py
@@ -9,6 +9,14 @@ from django.db.models import Count
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/index.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            return super().dispatch(request, *args, **kwargs)
+        except Exception as e:
+            import traceback
+            from django.http import HttpResponse
+            return HttpResponse(traceback.format_exc(), content_type='text/plain')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['page_title'] = 'VMMC ERP Dashboard'
